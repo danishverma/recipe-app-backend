d4518ed432e56ef24ac9a3c25150b43c5dbdbcf6
Danish Verma <danishverma0003@gmai.com>
2024-06-25 15:21:56 +0530

    initial commit for testing

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ config()
 const app = express()
 app.use(express.json());
 app.use(cors()) 
-const PORT = process.env.PORT
+import appRouting from './src/routes/index.js'
+appRouting(app);
+const PORT = process.env.PORT || 8800
 // const URL = process.env.DATABASE_URL
 const URL = process.env.MONGODB_URI
 
-app.use("/", router)
-console.log("=== mongod url ===",URL)
+// app.use("/", router)
+
 connect(URL).then(()=>{
+    app.get("/test", (req, res) => {
+        return res.json({message: 'BACKEND WORKING'})
+    })
     app.listen(PORT, ()=>{
-        console.log('Server is running on port:', PORT)
+        console.log(`server is running on http://localhost:${PORT}/api/v1`)
     })
 }).catch((err) => {
     console.error('Error connecting to MongoDB:', err);
--- src/routes/user-routes/user.routes.js
+++ src/routes/user-routes/user.routes.js
@@ -1,6 +1,13 @@
 import express from 'express'
-import userController from '../../controller/user.controller'
+// import userController from '../../controller/user.controller'
 
 const router = express.Router()
 
-router.get("/", userController.registerUser)
+// router.get("/", userController.registerUser)
+
+// router.post("/register", signUpMiddleware, userController.registerUser)
+// router.post("/login", userController.loginUser)
+
+router.get("/register", (req, res) => res.send("hello from register route"))
+router.get("/login", (req, res) => res.send("Hello from login route"))
+export default router;
