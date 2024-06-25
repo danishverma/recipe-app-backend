6ccae1a2c675d08787415ed13ca0633820cf7729
Danish Verma <danishverma0003@gmai.com>
2024-06-25 15:37:39 +0530

    Added login/signup handlers back

--- src/routes/user-routes/user.routes.js
+++ src/routes/user-routes/user.routes.js
@@ -1,13 +1,13 @@
 import express from 'express'
+import signUpMiddleware from '../../Middleware/user-middleware/users.middleware.js';
+import userController from '../../controller/user.controller.js';
 // import userController from '../../controller/user.controller'
 
 const router = express.Router()
 
 // router.get("/", userController.registerUser)
 
-// router.post("/register", signUpMiddleware, userController.registerUser)
-// router.post("/login", userController.loginUser)
+router.post("/register", signUpMiddleware, userController.registerUser)
+router.post("/login", userController.loginUser)
 
-router.get("/register", (req, res) => res.send("hello from register route"))
-router.get("/login", (req, res) => res.send("Hello from login route"))
 export default router;
