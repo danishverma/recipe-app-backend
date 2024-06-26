31b43c38bbe352612bd5931e636c1770dd89a6e8
Danish Verma <danishverma0003@gmai.com>
2024-06-26 09:50:08 +0530

    optimized contoller for register

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,14 +3,15 @@ import { generateResponse } from '../utils/utilityFunctions.js'
 
 const registerUser = async(req, res) => {
     try {
-        const userDetails = await userServices.registerUser(req.body)
-        return res.status(userDetails.status).send({
-            data: userDetails.data,
-            message: userDetails.message,
-            token: userDetails.auth
+        const userDetails = await userServices.registerUser(req.body).catch((error)=>{
+            throw {
+                statusCode: error?.statusCode ?? 500,
+                message: error?.message ??  "Internal server error"
+            }
         })
+        return generateResponse(res, userDetails)
     } catch (error) {
-        console.log('error in registering user', error)
+        return generateResponse(res, [],error.statusCode, error.message)
     }
 }
 
